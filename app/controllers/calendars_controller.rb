b232e65480fdb32dcccca53b4f189635b92551d5
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-03 17:59:33 +0900

    メソッド名をスネークケースへ変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,7 +1,7 @@
 class CalendarsController < ApplicationController
 
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_Week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     @todays_date = Date.today
